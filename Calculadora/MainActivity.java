49312b22e8b76fd0a578a8072476a68b72406db4
Victor Rodrigues Caetano <112587447+Bonbastic@users.noreply.github.com>
2023-03-24 21:37:38 -0300

    Add files via upload

---
+++ Calculadora/MainActivity.java
@@ -0,0 +1,36 @@
+package br.com.treinaweb.testes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText edtNum1, edtNum2;
+    TextView txtResultado;
+    Button btnCalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNum1 = (EditText) findViewById(R.id.editTextNum1);
+        edtNum2 = (EditText) findViewById(R.id.editTextNum2);
+        btnCalcular = (Button) findViewById(R.id.btnCalc);
+        txtResultado = (TextView) findViewById(R.id.textViewResultado);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int num1 = Integer.parseInt(edtNum1.getText().toString());
+        int num2 = Integer.parseInt(edtNum2.getText().toString());
+
+        Calculadora calc = new Calculadora(num1, num2);
+
+        txtResultado.setText(Integer.toString(calc.somar()));
+    }
+}
---
+++ Calculadora/MainActivityTest.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.Test
+    void onCreate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onClick() {
+    }
+}
